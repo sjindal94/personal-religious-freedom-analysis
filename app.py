987c0f99f099f3caf9f44548cd76fde9561a196d
Arun Swaminathan <arunswaminathan94@gmail.com>
2019-04-25 14:17:27 -0400

    Add endpoint to access terrorism data

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
-from constants import MAJORITY_POP_BY_YEAR
+from constants import MAJORITY_POP_BY_YEAR, TERRORISM_DB
 
 import pandas as pd
 import json
 
 app = Flask(__name__)
+
+
+@app.route("/terror_attacks/<year>", methods=['POST', 'GET'])
+def get_past_5_years_attacks(year):
+    df = pd.read_csv(TERRORISM_DB)
+    year_df = df[((df.iyear > int(year) - 5) & (df.iyear <= int(year)))].reset_index()
+
+    return json.dumps(year_df.values.tolist(), indent=2)
 
 
 @app.route("/map/<year>", methods=['POST', 'GET'])
--- constants.py
+++ constants.py
@@ -1,1 +1,2 @@
 MAJORITY_POP_BY_YEAR = 'data/world-religions/national_majority_data_by_year.csv'
+TERRORISM_DB = 'data/terrorism_database/terrorism_db.csv'
