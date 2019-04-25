150039ca4071d904deb4b98911a545e0496fbf7e
sjindal94 <shubhamjindal_1994@yahoo.com>
2019-04-25 15:54:21 -0400

    Adding Terror attacks on world map

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from constants import MAJORITY_POP_BY_YEAR, TERRORISM_DB
+from constants import MAJORITY_RELIGIOUS_POPULATION_DB, TERRORISM_DB
 
 import pandas as pd
 import json
@@ -11,15 +11,15 @@ app = Flask(__name__)
 def get_past_5_years_attacks(year):
     df = pd.read_csv(TERRORISM_DB)
     year_df = df[((df.iyear > int(year) - 5) & (df.iyear <= int(year)))].reset_index()
-
-    return json.dumps(year_df.values.tolist(), indent=2)
+    year_df = year_df[['country_txt', 'latitude', 'longitude']]
+    return json.dumps(year_df.iloc[:35].values.tolist(), indent=2)
 
 
 @app.route("/map/<year>", methods=['POST', 'GET'])
 def get_religion_by_year(year):
-    df = pd.read_csv(MAJORITY_POP_BY_YEAR)
+    df = pd.read_csv(MAJORITY_RELIGIOUS_POPULATION_DB)
     year_df = df[df.year == int(year)].reset_index()
-
+    year_df = year_df[['state', 'majority_religion', 'majority_population']]
     return json.dumps(year_df.values.tolist(), indent=2)
 
 
--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
-MAJORITY_POP_BY_YEAR = 'data/world-religions/national_majority_data_by_year.csv'
+MAJORITY_RELIGIOUS_POPULATION_DB = 'data/world-religions/national_majority_data_by_year.csv'
 TERRORISM_DB = 'data/terrorism_database/terrorism_db.csv'
