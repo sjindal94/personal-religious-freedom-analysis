dff61f4f759d939f775e70c093b0aaf1059b7265
Arun Swaminathan <arunswaminathan94@gmail.com>
2019-04-26 23:29:55 -0400

    Remove NAN from lat/lng. Sample data before sending them.

--- app.py
+++ app.py
@@ -12,7 +12,10 @@ def get_past_5_years_attacks(year):
     df = pd.read_csv(TERRORISM_DB)
     year_df = df[((df.iyear > int(year) - 5) & (df.iyear <= int(year)))].reset_index()
     year_df = year_df[['country_txt', 'latitude', 'longitude']]
-    return json.dumps(year_df.iloc[:35].values.tolist(), indent=2)
+    year_df = year_df[year_df['latitude'].notnull()]
+    # TODO: Adaptive sampling by country
+    year_df = year_df.sample(n=500)
+    return json.dumps(year_df.values.tolist(), indent=2)
 
 
 @app.route("/map/<year>", methods=['POST', 'GET'])
