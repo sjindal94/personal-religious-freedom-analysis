25910c9a0ec6b86c53ecbfa5ead4cb26b3242598
Arun Swaminathan <arunswaminathan94@gmail.com>
2019-04-30 17:20:34 -0400

    Add religion/freedom bar charts

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask, render_template
-from constants import MAJORITY_RELIGIOUS_POPULATION_DB, TERRORISM_DB
-
-import pandas as pd
 import json
 
+import pandas as pd
+from flask import Flask, render_template
+
+from constants import MAJORITY_RELIGIOUS_POPULATION_DB, TERRORISM_DB, PF_RELIGIOUS_FREEDOM
+
 app = Flask(__name__)
+
+
+@app.route("/all_religions", methods=['POST', 'GET'])
+def all_religions():
+    df = pd.read_csv(PF_RELIGIOUS_FREEDOM)
+
+    return json.dumps(df.religion.values.tolist(), indent=2)
+
+
+@app.route("/religious_freedom", methods=['POST', 'GET'])
+def get_religious_freedom():
+    df = pd.read_csv(PF_RELIGIOUS_FREEDOM)
+    res = list()
+
+    for val in df.values:
+        res.append({'religion': val[1], 'pf_religion': val[2], 'pf_religion_estop': val[3],
+                    'pf_religion_harrasment': val[4], 'pf_religion_restrictions': val[5]})
+
+    return json.dumps(res, indent=2)
 
 
 @app.route("/terror_attacks/<year>", methods=['POST', 'GET'])
@@ -24,6 +44,11 @@ def get_religion_by_year(year):
     year_df = df[df.year == int(year)].reset_index()
     year_df = year_df[['state', 'majority_religion', 'majority_population']]
     return json.dumps(year_df.values.tolist(), indent=2)
+
+
+@app.route("/religion", methods=['POST', 'GET'])
+def religion():
+    return render_template("religion.html")
 
 
 @app.route("/", methods=['POST', 'GET'])
--- constants.py
+++ constants.py
@@ -1,2 +1,3 @@
 MAJORITY_RELIGIOUS_POPULATION_DB = 'data/world-religions/national_majority_data_by_year.csv'
 TERRORISM_DB = 'data/terrorism_database/terrorism_db.csv'
+PF_RELIGIOUS_FREEDOM = 'data/human-freedom-index/religion_pf_freedom.csv'
